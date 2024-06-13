9be592007bdd59319ce99e7bde1d951d9b917055
Jack Brett <jdotbrett@gmail.com>
2024-06-13 12:02:34 +0100

    Node Signal Events
    Implemented node signal class  to listen for events from the Node execution system - intended to be used on the wiki and for developers to subscribe to nodes/orders and execute custom code.

---
+++ Assets/LUTE/Scripts/Orders/XRMenu.cs
@@ -0,0 +1,73 @@
+using MoreMountains.InventoryEngine;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[OrderInfo("XR",
+              "XRMenu",
+              "Toggles the XR Camera on/off using a custom button")]
+[AddComponentMenu("")]
+public class XRMenu : Order
+{
+    [Tooltip("Custom icon to display for this menu")]
+    [SerializeField] protected Sprite customButtonIcon;
+    [Tooltip("A custom popup class to use to display this menu - if one is in the scene it will be used instead")]
+    [SerializeField] protected PopupIcon setIconButton;
+    [Tooltip("If true, the popup icon will be displayed, otherwise it will be hidden")]
+    [SerializeField] protected bool showIcon = true;
+
+    [Header("XR Settings")]
+    [SerializeField]
+    public PlaneDetectionMode planeDetectionMode = PlaneDetectionMode.Horizontal;
+    [SerializeField]
+    public GameObject planeVisualiser;
+    [SerializeField]
+    public GameObject pointCloudVisualiser;
+
+    public override void OnEnter()
+    {
+        if (setIconButton != null)
+        {
+            PopupIcon.ActivePopupIcon = setIconButton;
+        }
+
+        var popupIcon = PopupIcon.GetPopupIcon();
+        if (popupIcon != null)
+        {
+            if (customButtonIcon != null)
+            {
+                popupIcon.SetIcon(customButtonIcon);
+            }
+        }
+        if (showIcon)
+        {
+            popupIcon.SetActive(true);
+        }
+
+        UnityEngine.Events.UnityAction action = () =>
+        {
+            //if the plane visualiser is not null, set it to the plane visualiser of the XR object
+            if (planeVisualiser != null)
+            {
+                var planeManager = XRHelper.getXRScript().gameObject.GetComponentInChildren<ARPlaneManager>();
+                planeManager.planePrefab = planeVisualiser;
+            }
+
+            //if the point cloud visualiser is not null, set it to the point cloud visualiser of the XR object
+            if (pointCloudVisualiser != null)
+            {
+                var pointCloudManager = XRHelper.getXRScript().gameObject.GetComponentInChildren<ARPointCloudManager>();
+                pointCloudManager.pointCloudPrefab = pointCloudVisualiser;
+            }
+        };
+        popupIcon.SetAction(action);
+        popupIcon.MoveToNextOption();
+
+        Continue();
+    }
+
+    public override string GetSummary()
+    {
+        return "Creates a button which will toggle the XR camera on/off";
+    }
+}
---
+++ Assets/LUTE/Scripts/Util/NodeSignals.cs
@@ -0,0 +1,23 @@
+
+using System.Security.Permissions;
+
+/// <summary>
+/// Use this class to define signals that can be used to communicate between nodes or be informed of events in node executions
+/// </summary>
+public static class NodeSignals
+{
+    public static event NodeStartHandler OnNodeStart;
+    public delegate void NodeStartHandler(Node node);
+
+    public static void NodeStart(Node node) { OnNodeStart?.Invoke(node); }
+
+    public static event NodeEndHandler OnNodeEnd;
+    public delegate void NodeEndHandler(Node node);
+
+    public static void NodeEnd(Node node) { OnNodeEnd?.Invoke(node); }
+
+    public static event OrderExecuteHandler OnOrderExecute;
+    public delegate void OrderExecuteHandler(Node node, Order order, int orderIndex, int maxOrderIndex);
+
+    public static void DoOrderExecute(Node node, Order order, int orderIndex, int maxOrderIndex) { OnOrderExecute?.Invoke(node, order, orderIndex, maxOrderIndex); }
+}
