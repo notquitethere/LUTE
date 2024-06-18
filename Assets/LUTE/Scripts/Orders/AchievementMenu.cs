6da62267ce9b7419acede664c4134b9fe11f21f7
Jack Brett <jdotbrett@gmail.com>
2024-06-18 15:23:34 +0100

    Implemented Achievement Toggle
    Added an order which allows you to create a toggle button which will show/hide an achievement list that either the designer supplies or is found in the scene
    Added a trophy texture to be used with the menu icon if desired

--- Assets/LUTE/Scripts/Orders/AchievementMenu.cs
+++ Assets/LUTE/Scripts/Orders/AchievementMenu.cs
@@ -1,18 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AchievementMenu : MonoBehaviour
+[OrderInfo("Achievements",
+              "Achievement List Button  ",
+              "Creates a button which will toggle the achievement on/off (rather than using a nested button in popups)")]
+[AddComponentMenu("")]
+public class AchievementMenu : Order
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Tooltip("Custom icon to display for this menu")]
+    [SerializeField] protected Sprite customButtonIcon;
+    [Tooltip("A custom popup class to use to display this menu - if one is in the scene it will be used instead")]
+    [SerializeField] protected PopupIcon setIconButton;
+    [Tooltip("The set achievement list to toggle - this will be found in the scene if not provided")]
+    [SerializeField] protected AchievementListFiller setList;
+    [Tooltip("If true, the popup icon will be displayed, otherwise it will be hidden")]
+    [SerializeField] protected bool showIcon = true;
 
-    // Update is called once per frame
-    void Update()
+    public override void OnEnter()
     {
-        
+        if(setList != null)
+        {
+            AchievementListFiller.ActiveList = setList;
+        }
+
+        var list = AchievementListFiller.GetList();
+
+        if (list == null)
+        {
+            Continue();
+            return;
+        }
+
+        if (setIconButton != null)
+        {
+            PopupIcon.ActivePopupIcon = setIconButton;
+        }
+
+        var popupIcon = PopupIcon.GetPopupIcon();
+        if (popupIcon != null)
+        {
+            if (customButtonIcon != null)
+            {
+                popupIcon.SetIcon(customButtonIcon);
+            }
+        }
+        if (showIcon)
+        {
+            popupIcon.SetActive(true);
+        }
+
+        UnityEngine.Events.UnityAction action = () =>
+        {
+            list.ShowList();
+        };
+
+        popupIcon.SetAction(action);
+        popupIcon.MoveToNextOption();
+
+        Continue();
+    }
+    public override string GetSummary()
+    {
+        return "Creates a button which will toggle the achievment list on/off (rather than using a nested button in popups)";
     }
 }
