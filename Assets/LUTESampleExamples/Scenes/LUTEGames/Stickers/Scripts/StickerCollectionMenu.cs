9c6d5aa72ad5c53e241e06338ff71fd40b93ed70
Jack Brett <jdotbrett@gmail.com>
2024-08-20 17:27:49 +0100

    Sticker Collection Menu
    Implemented basic menu which shows stickers that have been unlocked (i.e., found in inventory)
    TO DO:
    - Ensure scroll rect works as intended

--- Assets/LUTESampleExamples/Scenes/LUTEGames/Stickers/Scripts/StickerCollectionMenu.cs
+++ Assets/LUTESampleExamples/Scenes/LUTEGames/Stickers/Scripts/StickerCollectionMenu.cs
@@ -1,3 +1,6 @@
+using MoreMountains.InventoryEngine;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,18 +8,15 @@ namespace LoGaCulture.LUTE
 {
     public class StickerCollectionMenu : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-            var items = GetAllInstances<StickerItem>();
-            foreach (var item in items)
-            {
-                Debug.Log(item.ItemName);
+        [SerializeField] protected Inventory mainInventory;
+        [SerializeField] protected Transform stickerGroup;
 
-                // Have a locked and unlocked state
-                // Check against inventory to see if item exists then set state
-            }
-        }
+        protected LTDescr fadeTween; //Used for fading menu
+
+        protected static bool menuActive = false;
+
+        private CanvasGroup canvasGroup;
+        private List<StickerCollectionMenuItem> stickerItems = new List<StickerCollectionMenuItem>();
 
         public static T[] GetAllInstances<T>() where T : ScriptableObject
         {
@@ -29,6 +29,88 @@ namespace LoGaCulture.LUTE
             }
 
             return instances;
+        }
+
+        protected virtual void Start()
+        {
+            canvasGroup = GetComponentInChildren<CanvasGroup>();
+            if (!menuActive)
+            {
+                canvasGroup.alpha = 0.0f;
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+            }
+        }
+
+        public void ToggleMenu()
+        {
+            if (mainInventory == null) return;
+
+            var items = GetAllInstances<StickerItem>();
+            if (items.Length <= 0) return;
+
+            if (stickerGroup == null) return;
+
+            if (canvasGroup == null) return;
+
+            if (fadeTween != null)
+            {
+                LeanTween.cancel(fadeTween.id, true);
+                fadeTween = null;
+            }
+
+            foreach (var item in items)
+            {
+                bool unlocked = false;
+                if (stickerItems.Contains(stickerItems.Where(x => x.ItemID == item.ItemID).FirstOrDefault())) continue;
+                var stickerCollectionMenuItem = Instantiate(Resources.Load<GameObject>("Prefabs/StickerCollectionItem"), stickerGroup).GetComponent<StickerCollectionMenuItem>();
+                if (stickerCollectionMenuItem == null) continue;
+
+                stickerCollectionMenuItem.Initialise(item.ItemName, item.Icon, item.ItemID);
+                stickerItems.Add(stickerCollectionMenuItem);
+                // Check if the item is in the inventory
+                List<int> list = mainInventory.InventoryContains(item.ItemID);
+
+                if (list.Count > 0)
+                {
+
+                    unlocked = true;
+                }
+                stickerCollectionMenuItem.SetUnlocked(unlocked);
+            }
+
+            // Fade the canvas group in using lean tween
+            if (menuActive)
+            {
+                //Fade menu out
+                LeanTween.value(canvasGroup.gameObject, canvasGroup.alpha, 0f, 0.4f)
+        .setEase(LeanTweenType.easeOutQuint)
+        .setOnUpdate((t) =>
+        {
+            canvasGroup.alpha = t;
+        }).setOnComplete(() =>
+        {
+            canvasGroup.alpha = 0f;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+        });
+            }
+            else
+            {
+                //Fade menu in
+                LeanTween.value(canvasGroup.gameObject, canvasGroup.alpha, 1f, 0.4f)
+        .setEase(LeanTweenType.easeOutQuint)
+        .setOnUpdate((t) =>
+        {
+            canvasGroup.alpha = t;
+        }).setOnComplete(() =>
+        {
+            canvasGroup.alpha = 1f;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        });
+            }
+            menuActive = !menuActive;
         }
     }
 }
