320152e241ca6ffbe748914301ebf9b3ea440eab
Jack Brett <jdotbrett@gmail.com>
2024-10-01 12:43:05 +0100

    Inventory Slots
    - Updated inventory slots to be clickable which 'uses' or 'equips' them
    - Updated inventory slots to display a name which can be customised further
    - Added a slot prefab into Alby's folder which can be customised and is used to spawn the inventory slots on the inventory display canvas
    - Reset saves and all SOs

--- Assets/LUTE/ThirdPartyAssets/InventoryEngine/Editor/GUI/InventorySlotEditor.cs
+++ Assets/LUTE/ThirdPartyAssets/InventoryEngine/Editor/GUI/InventorySlotEditor.cs
@@ -1,59 +1,59 @@
-﻿using UnityEngine;
-using UnityEditor;
-using UnityEngine.UI;
-using System.Collections;
-using MoreMountains.InventoryEngine;
+﻿using UnityEditor;
 
 namespace MoreMountains.InventoryEngine
 {
-	[CustomEditor(typeof(InventorySlot))]
-	public class InventorySlotEditor : UnityEditor.UI.ButtonEditor 
-	{
-		protected SerializedProperty _movedSprite;
-		protected SerializedProperty _parentInventoryDisplay;
-		protected SerializedProperty _index;
-		protected SerializedProperty _slotEnabled;
-		protected SerializedProperty _targetImage;
-		protected SerializedProperty _targetCanvasGroup;
-		protected SerializedProperty _targetRectTransform;
-		protected SerializedProperty _iconRectTransform;
-		protected SerializedProperty _iconImage;
-		protected SerializedProperty _quantityText;
-        
-		protected override void OnEnable()
-		{
-			base.OnEnable();
-            
-			_movedSprite = serializedObject.FindProperty("MovedSprite");
-			_parentInventoryDisplay = serializedObject.FindProperty("ParentInventoryDisplay");
-			_index = serializedObject.FindProperty("Index");
-			_slotEnabled = serializedObject.FindProperty("SlotEnabled");
-			_targetImage = serializedObject.FindProperty("TargetImage");
-			_targetCanvasGroup = serializedObject.FindProperty("TargetCanvasGroup");
-			_targetRectTransform = serializedObject.FindProperty("TargetRectTransform");
-			_iconRectTransform = serializedObject.FindProperty("IconRectTransform");
-			_iconImage = serializedObject.FindProperty("IconImage");
-			_quantityText = serializedObject.FindProperty("QuantityText");
-		}
-        
-		public override void OnInspectorGUI() {
+    [CustomEditor(typeof(InventorySlot))]
+    public class InventorySlotEditor : UnityEditor.UI.ButtonEditor
+    {
+        protected SerializedProperty _movedSprite;
+        protected SerializedProperty _parentInventoryDisplay;
+        protected SerializedProperty _index;
+        protected SerializedProperty _slotEnabled;
+        protected SerializedProperty _targetImage;
+        protected SerializedProperty _targetCanvasGroup;
+        protected SerializedProperty _targetRectTransform;
+        protected SerializedProperty _iconRectTransform;
+        protected SerializedProperty _iconImage;
+        protected SerializedProperty _quantityText;
+        protected SerializedProperty _nameText;
 
-			base.OnInspectorGUI();
- 
-			EditorGUILayout.LabelField("Bindings", EditorStyles.boldLabel);
-			EditorGUILayout.ObjectField(_movedSprite);
-			EditorGUILayout.PropertyField(_parentInventoryDisplay);
-			EditorGUILayout.PropertyField(_index);
-			EditorGUILayout.PropertyField(_slotEnabled);
-			EditorGUILayout.PropertyField(_targetImage);
-			EditorGUILayout.PropertyField(_targetCanvasGroup);
-			EditorGUILayout.PropertyField(_targetRectTransform);
-			EditorGUILayout.PropertyField(_iconRectTransform);
-			EditorGUILayout.PropertyField(_iconImage);
-			EditorGUILayout.PropertyField(_quantityText);
- 
-			serializedObject.ApplyModifiedProperties();
+        protected override void OnEnable()
+        {
+            base.OnEnable();
 
-		}
-	}    
+            _movedSprite = serializedObject.FindProperty("MovedSprite");
+            _parentInventoryDisplay = serializedObject.FindProperty("ParentInventoryDisplay");
+            _index = serializedObject.FindProperty("Index");
+            _slotEnabled = serializedObject.FindProperty("SlotEnabled");
+            _targetImage = serializedObject.FindProperty("TargetImage");
+            _targetCanvasGroup = serializedObject.FindProperty("TargetCanvasGroup");
+            _targetRectTransform = serializedObject.FindProperty("TargetRectTransform");
+            _iconRectTransform = serializedObject.FindProperty("IconRectTransform");
+            _iconImage = serializedObject.FindProperty("IconImage");
+            _quantityText = serializedObject.FindProperty("QuantityText");
+            _nameText = serializedObject.FindProperty("NameText");
+        }
+
+        public override void OnInspectorGUI()
+        {
+
+            base.OnInspectorGUI();
+
+            EditorGUILayout.LabelField("Bindings", EditorStyles.boldLabel);
+            EditorGUILayout.ObjectField(_movedSprite);
+            EditorGUILayout.PropertyField(_parentInventoryDisplay);
+            EditorGUILayout.PropertyField(_index);
+            EditorGUILayout.PropertyField(_slotEnabled);
+            EditorGUILayout.PropertyField(_targetImage);
+            EditorGUILayout.PropertyField(_targetCanvasGroup);
+            EditorGUILayout.PropertyField(_targetRectTransform);
+            EditorGUILayout.PropertyField(_iconRectTransform);
+            EditorGUILayout.PropertyField(_iconImage);
+            EditorGUILayout.PropertyField(_quantityText);
+            EditorGUILayout.PropertyField(_nameText);
+
+            serializedObject.ApplyModifiedProperties();
+
+        }
+    }
 }
